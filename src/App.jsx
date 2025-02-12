affee595b11cb97ae17006ca8d952e7861f0b564
Kinkin <hiimkien2702@gmail.com>
2025-02-12 11:20:43 -0500

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Homepage from "./pages/Homepage"
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Homepage />
+      <Routes>
+        <Route index element={<Homepage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <header className="mb-20 flex items-center justify-between py-6">
+        <h1 className='flex flex-shrink-0 items-center'>
+          <Link to="/" >
+              <img className='mx-2 w-40' src={Logo} alt="logo" />
+          </Link>
+        </h1>
+            <div className='m-8 flex items-center justify-center gap-4 text-xl text-white'>
+            </div>
+    </header>
   )
 }
 
