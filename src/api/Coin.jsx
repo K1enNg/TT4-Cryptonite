df1476b6844290ea6870030766bac54c0db1d96c
Kinkin <hiimkien2702@gmail.com>
2025-02-28 19:28:48 -0500

    started implementing APIs

---
+++ src/api/Coin.jsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+
+export const CoinContext = createContext();
+
+const CoinContextProvider = (props) => {
+
+    const [coins, setCoins] = useState([]);
+    const [currency, setCurrency] = useState('usd');
+
+    const contextValue = {
+
+    }
+
+    return (
+        <CoinContext.Provider value={{}}>
+        {props.children}
+        </CoinContext.Provider>
+    );
+}
+
+export default CoinContextProvider;
