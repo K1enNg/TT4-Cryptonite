cf4585cd45043af1c57ddbd423b5f0fe87880c63
Kinkin <hiimkien2702@gmail.com>
2025-03-07 05:51:46 -0500

    removed dummy coin as we will be fetching the real coins from the API

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import Hero from '../components/Hero'
 import Coin from '../components/Coin'
-import TrendingCoin from '../components/TrendingCoin'
 
 const Homepage = () => {
 
   return (
     <>
-    <TrendingCoin />
       {/* <Hero /> */}
       <Coin />
     </>
