48cef0bc03b9c138d2f286efe5f300a8434c38e5
Kinkin <hiimkien2702@gmail.com>
2025-04-14 09:08:38 -0400

    added path file in node server

--- server/config.js
+++ server/config.js
@@ -1,7 +1,9 @@
 export const PORT = 5000;
 
-export const mongoDBURL = 
-'mongodb+srv://admin:admin@api.c1fqmy6.mongodb.net/api?retryWrites=true&w=majority&appName=API'
+// export const mongoDBURL = 
+// 'mongodb+srv://admin:admin@api.c1fqmy6.mongodb.net/api?retryWrites=true&w=majority&appName=API'
+
+export const mongoDBURL = process.env.CONN_STR;
 
 export const newsAPIkey = 'pub_774696f9fde9b83241904690759e3448ffb2f';
 
